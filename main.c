ba24005d0e6bd14d90d115de5b31aa9874142951
LuHui <luhux@foxmail.com>
2026-03-22 22:16:15 +0800

    MAIN: ADD MISSING CODE

--- main.c
+++ main.c
@@ -1,17 +1,47 @@
 #include "config.h"
 #include "HAL.h"
 #include "usbdev.h"
+#include "main.h"
 #include "debug.h"
+
+uint8_t main_taskid;
 
 __attribute__((aligned(4))) uint32_t MEM_BUF[BLE_MEMHEAP_SIZE / 4];
 
 __HIGH_CODE
-__attribute__((noinline))
-void Main_Circulation()
+__attribute__((noinline)) void Main_Circulation()
 {
 	while (1) {
 		TMOS_SystemProcess();
+		usbdev_task();
 	}
+}
+
+uint16_t Main_ProcessEvent(uint8_t task_id, uint16_t events)
+{
+	if (events & SYS_EVENT_MSG) {
+		uint8_t *pMsg;
+
+		if ((pMsg = tmos_msg_receive(main_taskid)) != NULL) {
+			// Release the TMOS message
+			tmos_msg_deallocate(pMsg);
+		}
+		// return unprocessed events
+		return (events ^ SYS_EVENT_MSG);
+	}
+
+	if (events & MAIN_EVT_START) {
+		tmos_start_task(main_taskid, MAIN_EVT_PERIODIC1S, 1600);
+		return (events ^ MAIN_EVT_START);
+	}
+
+	if (events & MAIN_EVT_PERIODIC1S) {
+		usbdev_show_stat();
+		tmos_start_task(main_taskid, MAIN_EVT_PERIODIC1S, 1600);
+		return (events ^ MAIN_EVT_PERIODIC1S);
+	}
+
+	return 0;
 }
 
 int main(void)
@@ -33,5 +63,7 @@ int main(void)
 	HAL_Init();
 	usbdev_init();
 	debug_puts("CH582 USB BRIDGE\r\n");
+	main_taskid = TMOS_ProcessEventRegister(Main_ProcessEvent);
+	tmos_set_event(main_taskid, MAIN_EVT_START);
 	Main_Circulation();
 }
