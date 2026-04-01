da120ed10e481b2c78d595f1550f688857aeb074
LuHui <luhux@foxmail.com>
2026-04-01 22:19:34 +0800

    UART: INIT UART0

--- uart.c
+++ uart.c
@@ -14,6 +14,24 @@ struct fifo8 uart1_rxfifo = {
 	.tail = 0,
 };
 
+static int uart0_trig_bytes = 4;
+
+__aligned(4) uint8_t uart0_rxfifo_buf[256];
+struct fifo8 uart0_rxfifo = {
+	.buf = &uart0_rxfifo_buf[0],
+	.mask = 256 - 1,
+	.head = 0,
+	.tail = 0,
+};
+
+__aligned(4) uint8_t uart0_txfifo_buf[256];
+struct fifo8 uart0_txfifo = {
+	.buf = &uart0_txfifo_buf[0],
+	.mask = 256 - 1,
+	.head = 0,
+	.tail = 0,
+};
+
 void uart_task(void)
 {
 	if (R8_UART1_TFC == 0) {
@@ -40,6 +58,33 @@ void uart_init(void)
 	UART1_ByteTrigCfg(UART_4BYTE_TRIG);
 	UART1_INTCfg(ENABLE, RB_IER_RECV_RDY);
 	PFIC_EnableIRQ(UART1_IRQn);
+
+	/*
+	  DEFAULT UART0 PIN:
+
+	  TXD: PB7
+	  RXD: PB4
+	  RTS: PB6
+	  CTS: PB0
+	 */
+	fifo8_reset(&uart0_rxfifo);
+	fifo8_reset(&uart0_txfifo);
+	GPIOB_SetBits(bTXD0);
+	GPIOB_ModeCfg(bTXD0, GPIO_ModeOut_PP_5mA);
+	GPIOB_ModeCfg(bRXD0, GPIO_ModeIN_PU);
+	GPIOB_ModeCfg(bRTS, GPIO_ModeOut_PP_5mA);
+	GPIOB_ModeCfg(bCTS, GPIO_ModeIN_PU);
+	UART0_BaudRateCfg(1200);
+	R8_UART0_MCR |= RB_MCR_AU_FLOW_EN;
+	R8_UART0_FCR = (2 << 6) | RB_FCR_TX_FIFO_CLR | RB_FCR_RX_FIFO_CLR |
+		       RB_FCR_FIFO_EN; // FIFO打开，触发点4字节
+	R8_UART0_LCR = RB_LCR_WORD_SZ;
+	R8_UART0_IER = RB_IER_TXD_EN | RB_IER_RTS_EN;
+	R8_UART0_DIV = 1;
+
+	UART0_ByteTrigCfg(UART_4BYTE_TRIG);
+	UART0_INTCfg(ENABLE, RB_IER_RECV_RDY);
+	PFIC_EnableIRQ(UART0_IRQn);
 }
 
 __INTERRUPT
@@ -58,6 +103,33 @@ void UART1_IRQHandler(void)
 	case UART_II_RECV_TOUT:
 		while (R8_UART1_RFC) {
 			fifo8_push(&uart1_rxfifo, R8_UART1_RBR);
+		}
+		break;
+	case UART_II_THR_EMPTY:
+		break;
+	case UART_II_MODEM_CHG:
+		break;
+	default:
+		break;
+	}
+}
+
+__INTERRUPT
+__HIGH_CODE
+void UART0_IRQHandler(void)
+{
+	switch (UART0_GetITFlag()) {
+	case UART_II_LINE_STAT:
+		UART0_GetLinSTA();
+		break;
+	case UART_II_RECV_RDY:
+		for (int i = 0; i != uart0_trig_bytes; i++) {
+			fifo8_push(&uart0_rxfifo, UART0_RecvByte());
+		}
+		break;
+	case UART_II_RECV_TOUT:
+		while (R8_UART0_RFC) {
+			fifo8_push(&uart0_rxfifo, R8_UART0_RBR);
 		}
 		break;
 	case UART_II_THR_EMPTY:
